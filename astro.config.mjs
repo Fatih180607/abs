ced1b4d13f98e9c539b902d973546f46613d99c2
jose <joseantonio161206@gmail.com>
2024-09-07 13:11:02 +0700

    menambahkan fitur search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
-export default defineConfig({
-  // Astro configuration goes here
+// https://astro.build/config
+export default defineConfig(
+  {
+    output: "hybrid",
+    adapter: node({
+      mode: "standalone",
+    }),
+    devToolbar: {
+      enabled: false,
+    },
+  } // Astro configuration goes here
   // Ensure no server URLs or properties are accessed here prematurely
-});
+);
