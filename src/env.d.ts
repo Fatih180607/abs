4599c6964f2a0c6d29377a680612a39c5f544254
jose <joseantonio161206@gmail.com>
2024-09-07 10:52:40 +0700

    ambil data dari supabase

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,1 @@
 /// <reference types="astro/client" />
-SUPABASE_URL = "https://xlxckzgmuxkijtcjmvee.supabase.co";
-SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseGNremdtdXhraWp0Y2ptdmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ4OTI2NDYsImV4cCI6MjA0MDQ2ODY0Nn0.-6GkzaSkPmmRiKSgENnItHMKbKLC-BErY52chZlGnq0";
