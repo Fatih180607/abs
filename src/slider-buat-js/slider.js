9949c3a6fa8a8e81ea26665c25c1ea9054519e6d
jose <joseantonio161206@gmail.com>
2024-09-11 15:23:04 +0700

    menganti warna background

--- src/slider-buat-js/slider.js
+++ src/slider-buat-js/slider.js
@@ -1,64 +1,15 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-// Event listeners for buttons
-// document.querySelector(".prev").addEventListener("click", () => {
-//   plusSlides(-1);
-// });
-
-// document.querySelector(".next").addEventListener("click", () => {
-//   plusSlides(1);
-// });
-
-// Event listeners for dots
-const dots = document.querySelectorAll(".dot");
-dots.forEach((dot, index) => {
-  dot.addEventListener("click", () => {
-    currentSlide(index + 1);
-  });
-});
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
+function showSlides() {
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  // Hide all slides
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
-  // Remove active class from all dots
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
   }
-
-  // Show the selected slide
   slides[slideIndex - 1].style.display = "block";
-
-  // Add active class to the corresponding dot
-  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 3000); // Change slide every 3 seconds
 }
-
-// Optional: Automatic Slideshow
-function autoSlides() {
-  plusSlides(1);
-  setTimeout(autoSlides, 5000); // Change slide every 5 seconds
-}
-
-// Start the automatic slideshow
-autoSlides();
